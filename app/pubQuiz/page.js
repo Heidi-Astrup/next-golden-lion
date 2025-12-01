7ae2087de0687796b0643c32d70066cd54b0b717
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-12-01 09:17:46 +0100

    pub quiz page started - fixed fonts

--- app/pubQuiz/page.js
+++ app/pubQuiz/page.js
@@ -2,11 +2,12 @@ export default function PubQuiz() {
   return (
     <div className="min-h-screen pt-20 pb-10 px-5 flex items-center justify-center">
       <main className="text-center max-w-[600px]">
-        <h1 className="text-[32px] font-semibold mb-4 tracking-tight text-[#ffffff]">
-          Pub Quiz
+        <h1 className="text-5xl font-heading text-center font-semibold mb-4 tracking-tight text-[#E5A702]">
+          PUB QUIZ
         </h1>
-        <p className="text-base text-gray-400 mb-8 leading-relaxed">
-          Quiz here
+        <p className="text-3xl font-heading text-center text-[#E5A702] mb-8 leading-relaxed">
+          Join the fun, answer some hilarious questions, while enjoying you
+          favourite drinks <br></br> AND win free drink vouchers (up to 250kr)
         </p>
       </main>
     </div>
