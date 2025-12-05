e368e475521a857df0138123a7dbe50a37ce03c3
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-12-05 15:58:13 +0100

    plus and minus bottons on beverages-card in basket

--- components/BeverageCard.js
+++ components/BeverageCard.js
@@ -31,6 +31,22 @@ const BeverageCard = ({ beverages, basket, basketItems, setBasketItems }) => {
     setBasketItems(updatedArray);
   }
 
+  function minus() {
+    const updatedArray = [...basketItems];
+    const index = updatedArray.findIndex((item) => item.id === beverages.id);
+    if (index !== -1) updatedArray.splice(index, 1);
+    localStorage.setItem("beverageName", JSON.stringify(updatedArray));
+    setBasketItems(updatedArray);
+  }
+
+  function plus() {
+    const updatedArray = [...basketItems];
+    const index = updatedArray.findIndex((item) => item.id === beverages.id);
+    if (index !== -1) updatedArray.splice(index, 1);
+    localStorage.setItem("beverageName", JSON.stringify(updatedArray));
+    setBasketItems(updatedArray);
+  }
+
   if (basket === "inBasket") {
     return (
       <div className="text-left text-[#FFF5D6] w-44 h-80 border-2 border-[#E5A702] rounded-xl p-4 flex flex-col justify-between">
@@ -46,14 +62,33 @@ const BeverageCard = ({ beverages, basket, basketItems, setBasketItems }) => {
           {beverages.quantity > 1 ? `x${beverages.quantity}` : ""}
         </h3>
         <p className="font-body text-[20px]">{beverages.price}</p>
-        <Image
-          className="w-8 h-10 self-end"
-          src={trashCan}
-          alt="Trash Can"
-          width={50}
-          height={50}
-          onClick={trashIt}
-        ></Image>
+        <div className="flex justify-between">
+          <div className="flex justify-between gap-3 px-2 border-2 border-[#E5A702] rounded-lg">
+            <h3
+              onClick={minus}
+              className="text-4xl text-[#E5A702] font-heading leading-none"
+            >
+              -
+            </h3>
+            <h3 className="text-3xl text-[#E5A702] font-heading leading-none mt-1.5">
+              {beverages.quantity}
+            </h3>
+            <h3
+              onClick={plus}
+              className="text-4xl text-[#E5A702] font-heading leading-none"
+            >
+              +
+            </h3>
+          </div>
+          <Image
+            className="w-8 h-10"
+            src={trashCan}
+            alt="Trash Can"
+            width={50}
+            height={50}
+            onClick={trashIt}
+          ></Image>
+        </div>
       </div>
     );
   } else {
