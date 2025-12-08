6452808c0f55fb90f414e78bf8fa6560169216bc
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-12-08 14:21:07 +0100

    added a component for staff page, to simplyfy the staff/id/page.js

---
+++ components/OrderBox.js
@@ -0,0 +1,78 @@
+import OrderCard from "@/components/OrderCard";
+
+export default function OrderBox({ orders }) {
+  async function orderDone(orderId) {
+    const url = `${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}/order/${orderId}.json`; // Get Firebase Realtime Database URL
+
+    await fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify({
+        isDone: true,
+      }),
+    });
+
+    setOrders((prev) => prev.filter((order) => order.id !== orderId));
+  }
+
+  async function orderCanceled(orderId) {
+    const url = `${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}/order/${orderId}.json`; // Get Firebase Realtime Database URL
+
+    await fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify({
+        isCanceled: true,
+      }),
+    });
+
+    setOrders((prev) => prev.filter((order) => order.id !== orderId));
+  }
+
+  return (
+    <div>
+      {orders.length === 0 ? (
+        <p className="text-gray-400">Ingen ordrer endnu.</p>
+      ) : (
+        <div className="flex flex-col gap-10">
+          {orders.map((order) => (
+            <div
+              key={order.id}
+              className="text-left border-4 border-[#E5A702] p-4 rounded-2xl"
+            >
+              <p className="text-[#E5A702] text-2xl p-4">
+                Order ID: {order.id}
+              </p>
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {order.beverages && order.beverages.length > 0 ? (
+                  order.beverages.map((beverages) => (
+                    <OrderCard
+                      key={beverages.id}
+                      beverages={beverages}
+                      phone={order.phone}
+                      name={order.name}
+                    />
+                  ))
+                ) : (
+                  <p className="text-white">Ingen ordrer endnu.</p>
+                )}
+              </div>
+              <div className="flex flex-col gap-4 mt-8">
+                <button
+                  onClick={() => orderDone(order.id)}
+                  className="bg-[#E5A702] w-4/4 font-bold p-2 rounded-[10px] text-black text-3xl"
+                >
+                  DONE
+                </button>
+                <button
+                  onClick={() => orderCanceled(order.id)}
+                  className="bg-black w-4/4 font-bold p-2 border-2 border-[#E5A702] rounded-[10px] text-[#E5A702] text-3xl"
+                >
+                  CANCEL
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
